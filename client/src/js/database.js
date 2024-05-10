c1d26ab3244d59550962a3fb872f597b7cdf601b
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-05-10 09:33:09 -0400

    basic AC done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -21,7 +21,7 @@ export const putDb = async (content) => {
   const request = store.add({ content: content }); 
   const result = await request;
   console.log('Data saved to the database', result);  
-  console.error('putDb not implemented')
+
 };
 
 // TODO: Add logic for a method that gets all the content from the database
